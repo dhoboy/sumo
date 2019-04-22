50a502fb6c1fe80048fbc14708ff0fd0089ec143
Daniel Overstreet <overstreet@collectivedigitalstudio.com>
2019-04-21 20:28:57 -0700

    Updates

---
+++ precache-manifest.7f32c60a63f6c8f650fc38ca620c8584.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45574ec625eef6495191",
+    "url": "/sumo/static/css/main.ae726650.chunk.css"
+  },
+  {
+    "revision": "45574ec625eef6495191",
+    "url": "/sumo/static/js/main.d4dafc97.chunk.js"
+  },
+  {
+    "revision": "5bd8929ae0dda8d286d9",
+    "url": "/sumo/static/js/runtime~main.677aabb2.js"
+  },
+  {
+    "revision": "a2a3ef21e26cfc5ce30a",
+    "url": "/sumo/static/js/2.61dd37be.chunk.js"
+  },
+  {
+    "revision": "c99165fc4eca0da4af113462539e0b28",
+    "url": "/sumo/index.html"
+  }
+];
